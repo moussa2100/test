f5631f753f8039ed7da1f06e21cffd076ee524ef
Mohamed Moussa <moussa2100@gmail.com>
2020-02-18 14:10:39 +0300

    Updates

---
+++ precache-manifest.0dcbcd6fa6088f02175e47f72f186b4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1bf74445a5ee6e9d602ce1d5e41c8d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "e14ab115c682fd78a52a",
+    "url": "/test/static/css/main.3030d6a3.chunk.css"
+  },
+  {
+    "revision": "9278dc906be69e54f01f",
+    "url": "/test/static/js/2.5cef16ab.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/test/static/js/2.5cef16ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14ab115c682fd78a52a",
+    "url": "/test/static/js/main.fa47aaa0.chunk.js"
+  },
+  {
+    "revision": "31cd570d2d6c532ea457",
+    "url": "/test/static/js/runtime-main.0522d865.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);

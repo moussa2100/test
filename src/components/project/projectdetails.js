d9d3de5d82795f184f21fecf39055cbd14aae20f
Mohamed Moussa <moussa2100@gmail.com>
2020-02-18 14:57:01 +0300

    3

--- src/components/project/projectdetails.js
+++ src/components/project/projectdetails.js
@@ -6,7 +6,7 @@ function projectdetails() {
          
          <div className='card  details'>
             <div className='card-content'>
-                <span className='card-title' > Cardd Title update 2 </span>
+                <span className='card-title' > Cardd Title update 3 </span>
                 <p>There are 4 colors and 3 sizes of circular spinners. The spinner should be nested in a preloader-wrapper div. The default size is medium, but you can add the classes big or small to adjust the size accordingly. You can add the classes spinner-red-only, spinner-blue-only, spinner-yellow-only and spinner-green-only. You can also leave this class as just  </p>
             </div>
          </div>
